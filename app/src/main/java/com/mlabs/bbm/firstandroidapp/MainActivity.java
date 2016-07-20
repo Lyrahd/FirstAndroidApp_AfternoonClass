68082894603196bd7b265f24aa85ba0910e115f7
wynrar <alwyn.villanueva@adamson.edu.ph>
2016-07-21 06:54:28 +0800

    Modified Splash Screen. Added rotate.xml. Added Login Screen. (Validates Email and Password Using Regex)

--- app/src/main/java/com/mlabs/bbm/firstandroidapp/MainActivity.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp/MainActivity.java
@@ -1,13 +1,47 @@
 package com.mlabs.bbm.firstandroidapp;
 
+
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn;
+        final EditText emailAdd,pwd;
+
+        emailAdd=(EditText)findViewById(R.id.editText);
+        pwd=(EditText)findViewById(R.id.editText2);
+        btn = (Button)findViewById(R.id.button);
+        if (btn!=null){
+            btn.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public  void onClick(View v){
+
+
+                        if (Pattern.compile("^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$").matcher(emailAdd.getText()).matches() && (Pattern.compile("([a-zA-Z0-9]+_?)+").matcher(pwd.getText()).matches() && pwd.getText().length()>=6)) {
+                            Intent intent = new Intent(MainActivity.this, blankAct.class);
+                            startActivity(intent);
+                            Toast.makeText(getBaseContext(), "Login successful!", Toast.LENGTH_SHORT).show();
+                        } else
+                            Toast.makeText(getBaseContext(), "Login Failed!", Toast.LENGTH_SHORT).show();
+
+
+                }
+            });
+        }
     }
+
+
 }
--- app/src/main/java/com/mlabs/bbm/firstandroidapp/SplashScreen.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp/SplashScreen.java
@@ -3,6 +3,9 @@ package com.mlabs.bbm.firstandroidapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 /**
  * Created by benjarmanalili on 16/07/2016.
@@ -14,21 +17,29 @@ public class SplashScreen extends Activity{
     super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
 
-    Thread timerThread = new Thread(){
-        public void run(){
-            try{
-                sleep(3000);
+    final ImageView iv = (ImageView) findViewById(R.id.imageView3);
+    final Animation an = AnimationUtils.loadAnimation(getBaseContext(),R.anim.rotate);
+
+        iv.startAnimation(an);
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }
+                catch(InterruptedException e){
+                    e.printStackTrace();
+                }
+                finally{
+
+                    Intent intent = new Intent(SplashScreen.this,MainActivity.class );
+                    startActivity(intent);
+
+
+                }
             }
-            catch(InterruptedException e){
-                e.printStackTrace();
-            }
-            finally{
-                Intent intent = new Intent(SplashScreen.this,MainActivity.class );
-                startActivity(intent);
-            }
-        }
-    };
+        };
         timerThread.start();
+
     }
 
     @Override
@@ -36,4 +47,5 @@ public class SplashScreen extends Activity{
         super.onPause();
         finish();
     }
+
 }
