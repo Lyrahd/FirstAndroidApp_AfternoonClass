32859d76a9031f367d6da494f095f78b0e2f5212
Tim Calilao <calilao20@gmail.com>
2016-07-21 21:55:02 +0800

    Test

---
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp/login.java
@@ -0,0 +1,52 @@
+package com.mlabs.bbm.firstandroidapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+/**
+ * Created by Tim on 7/21/2016.
+ */
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+        Button Login;
+        final EditText txtEmail;
+        final EditText txtPass;
+        txtPass = (EditText)findViewById((R.id.txtPass));
+        txtEmail = (EditText)findViewById((R.id.txtEmail));
+        Login = (Button)findViewById(R.id.btnLogin);
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    if (Patterns.EMAIL_ADDRESS.matcher(txtEmail.getText()).matches())
+                    {
+                        if (txtPass.length()>=8)
+                        {
+                            Intent intent = new Intent(login.this,afterlogin.class );
+                            startActivity(intent);
+                        }
+                        else
+                        {
+                            Toast.makeText(getBaseContext(),"Password must be at least 8 characters.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                else
+                    {
+                        Toast.makeText(getBaseContext(),"Please enter a valid e-mail address.", Toast.LENGTH_SHORT).show();
+                    }
+            }
+        });
+    }
+}
+
