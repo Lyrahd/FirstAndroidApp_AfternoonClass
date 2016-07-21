3cc7b7bcbc739f05e14fc2fcbac4dce7a65071c7
fkeifer <kffababeir@gmail.com>
2016-07-21 23:29:01 +0800

    Changed splash screen image

--- app/src/main/java/com/mlabs/bbm/firstandroidapp/SplashScreen.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp/SplashScreen.java
@@ -2,7 +2,15 @@ package com.mlabs.bbm.firstandroidapp;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by benjarmanalili on 16/07/2016.
@@ -11,13 +19,13 @@ public class SplashScreen extends Activity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
 
-    Thread timerThread = new Thread(){
+        Thread timerThread = new Thread(){
         public void run(){
             try{
-                sleep(3000);
+                sleep(4000);
             }
             catch(InterruptedException e){
                 e.printStackTrace();
