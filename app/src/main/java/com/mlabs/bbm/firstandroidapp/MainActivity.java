919a5817932c6d6dd8aabc13b17b8d9975cb9a09
HAirineo <homer.irineo@gmail.com>
2016-07-21 17:57:41 +0800

    Added email and password validation, Added login toast

--- app/src/main/java/com/mlabs/bbm/firstandroidapp/MainActivity.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp/MainActivity.java
@@ -2,12 +2,59 @@ package com.mlabs.bbm.firstandroidapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public final static boolean isValidEmail(CharSequence target) {
+        if (TextUtils.isEmpty(target)) {
+            return false;
+        } else {
+            return android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
+        }
+    }
+
+    private Toast popToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText email = (EditText) findViewById(R.id.email);
+        final EditText pass = (EditText) findViewById(R.id.pass);
+        final Button login = (Button) findViewById(R.id.login);
+
+        popToast = Toast.makeText(getApplicationContext(), null, Toast.LENGTH_SHORT);
+
+
+        login.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                if(isValidEmail(email.getText())==false)
+                {
+                    email.setError("Please enter a valid email address.");
+                }
+                else if(pass.getText().length()<8)
+                {
+                    pass.setError("Minimum password length is at least 8 characters.");
+                }
+                else
+                {
+                    popToast.setText("Logged in!");
+                    popToast.show();
+                }
+            }
+
+        });
+
+
+
     }
 }
