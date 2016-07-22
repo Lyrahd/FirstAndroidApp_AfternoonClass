ad868fab2c74e81ae0af3e4128a6c97c73cfdb44
TanghalJohnElmer <johnelmerm.tanghal@yahoo.com>
2016-07-22 14:42:45 +0800

    Recommited my app because I accidentally uploaded it from my classmate's account while im using his pc

---
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp/form.java
@@ -0,0 +1,16 @@
+package com.mlabs.bbm.firstandroidapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by User on 7/22/2016.
+ */
+public class form extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.formlay);
+    }
+
+}
