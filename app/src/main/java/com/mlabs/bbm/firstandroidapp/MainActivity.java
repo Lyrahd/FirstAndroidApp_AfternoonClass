dbc59e161d48a533b1b58dd9a65f3c9dc7f04182
TanghalJohnElmer <johnelmerm.tanghal@yahoo.com>
2016-07-22 14:36:41 +0800

    Recommited the branch because i used my classmate's pc and accidentally uploaded it from his account

--- app/src/main/java/com/mlabs/bbm/firstandroidapp/MainActivity.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp/MainActivity.java
@@ -1,76 +1,51 @@
 package com.mlabs.bbm.firstandroidapp;
 
-import android.content.DialogInterface;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.AlertDialog;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import  android.content.Intent;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
 import java.util.regex.Pattern;
 
 
-
-public class MainActivity extends ActionBarActivity {
-
-
-    EditText Uname;
-    EditText Pwd;
-    Button btnLogs;
-
-
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button Btnlogin ;
+        final EditText EmailADD, PwD;
 
-    }
+        EmailADD = (EditText)findViewById(R.id.editText);
+        PwD = (EditText)findViewById(R.id.editText2);
+        Btnlogin = (Button)findViewById(R.id.button);
+        if (Btnlogin != null) {
+            Btnlogin.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (Pattern.compile("([a-zA-Z0-9]+_?)+@[a-zA-Z0-9]+\\.com").matcher(EmailADD.getText()).matches()) {
+                        if (!(PwD.length() == 0)) {
+                            if (PwD.length() > 8) {
+                                Intent intent = new Intent(MainActivity.this, form.class);
+                                startActivity(intent);
+                            } else
+                                Toast.makeText(getBaseContext(), "Password too short", Toast.LENGTH_SHORT).show();
+                        } else
+                            Toast.makeText(getBaseContext(), "Password field is empty", Toast.LENGTH_SHORT).show();
+                    } else
+                        Toast.makeText(getBaseContext(), "Invalid Email Address", Toast.LENGTH_SHORT).show();
 
-    public void onButtonClick(View v)
-    {
-        if(v.getId() ==  R.id.btnLog)
-        {
-            EditText a = (EditText)findViewById(R.id.TFUname);
-            EditText p = (EditText)findViewById(R.id.TFPword);
-            Intent i = new Intent(MainActivity.this, Display.class);
 
-            String uname = a.getText().toString();
-            String pword = p.getText().toString();
-
-            boolean b = Pattern.matches("[a-zA-Z0-9]{4,16}", uname);
-            if(b == true)
-            {
-
-                i.putExtra("Username",uname);
-                startActivity(i);
-            }
-            else
-            {
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setMessage(" Username or Password Must Be :" +  "\n" +
-                        " ALPHANUMERIC" + "\n" +
-                        " 8 to 24 Characters");
-                builder.setPositiveButton("Okay", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.cancel();
-
-                    }
-                });
-                AlertDialog alert = builder.create();
-                alert.show();
-
-            }
+                    /*Toast.makeText(getBaseContext(),, Toast.LENGTH_SHORT).show();*/
+                }
+            });
         }
-    }
 
-    public void clickExit(View x)
-    {
-        finish();
-    }
-
+        }
 }
 
 
