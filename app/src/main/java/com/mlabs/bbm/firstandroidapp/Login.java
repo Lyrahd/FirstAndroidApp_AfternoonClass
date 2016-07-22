3cf16a735d31aa35a7f58cfbc2eec1c18dece85a
YvetteKiak <yvetteprivate@gmail.com>
2016-07-22 20:11:54 +0800

    done

---
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp/Login.java
@@ -0,0 +1,16 @@
+package com.mlabs.bbm.firstandroidapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Alien on 7/22/2016.
+ */
+public class Login extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
+
