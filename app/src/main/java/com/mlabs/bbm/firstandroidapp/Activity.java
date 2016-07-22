4f1903ea4b8741f6ea8e1d9ac3f2919ff1811128
Roque Pospos <Roque Pospos>
2016-07-22 16:59:47 +0800

    final commit w/ UI improvements

---
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp/Activity.java
@@ -0,0 +1,20 @@
+package com.mlabs.bbm.firstandroidapp;
+
+/**
+ * Created by rpospos on 7/20/2016.
+ */
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Activity extends android.app.Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity);
+
+
+
+    }
+}
--- app/src/main/java/com/mlabs/bbm/firstandroidapp/MainActivity.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp/MainActivity.java
@@ -2,12 +2,76 @@ package com.mlabs.bbm.firstandroidapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    public static boolean validationEm(CharSequence email) {
+        if (TextUtils.isEmpty(email)) {
+            return false;
+        } else {
+            return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
+        }
+    }
+
+    Button btn1;
+    EditText txt_mail,txt_pw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn1=(Button)findViewById(R.id.button);
+        txt_mail=(EditText)findViewById(R.id.txt_email);
+        txt_pw=(EditText)findViewById(R.id.txt_pw);
+
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                txt_mail.setError(null);
+                txt_pw.setError(null);
+
+                if(validationEm(txt_mail.getText())==true && txt_pw.getText().length()>0)
+                {
+                    Intent i;
+                    i = new Intent(MainActivity.this, Activity.class);
+                    startActivity(i);
+                }
+
+                if(validationEm(txt_mail.getText())==false)
+                {
+                    txt_mail.setError(getString(R.string.error_em));
+                }
+
+
+                else if(txt_mail.getText().toString().equals("") &&
+
+                        txt_pw.getText().toString().equals("")) {
+                    txt_mail.setError(getString(R.string.error));
+                    txt_pw.setError(getString(R.string.error));
+                }
+
+                else if(txt_mail.getText().toString().equals("")) {
+                    txt_mail.setError(getString(R.string.error));
+
+                }
+
+                else if(txt_pw.getText().toString().equals("")) {
+                    txt_pw.setError(getString(R.string.error));
+                }
+
+            }
+        });
+
+
     }
+
+
 }
