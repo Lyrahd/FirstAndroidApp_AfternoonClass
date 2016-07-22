fd17ffb3eac77ec095d9d26022df02ec47095396
Jzauwne Ruzzl Leoj S. Abalus <jzauwne.ruzzl.leoj.abalus@adamson.edu.ph>
2016-07-22 15:13:27 +0800

    Added Splash Screen

--- app/src/main/java/com/mlabs/bbm/firstandroidapp/MainActivity.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp/MainActivity.java
@@ -14,12 +14,6 @@ import java.util.regex.Pattern;
 
 public class MainActivity extends ActionBarActivity {
 
-
-    EditText Uname;
-    EditText Pwd;
-    Button btnLogs;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,50 +21,6 @@ public class MainActivity extends ActionBarActivity {
 
 
     }
-
-    public void onButtonClick(View v)
-    {
-        if(v.getId() ==  R.id.btnLog)
-        {
-            EditText a = (EditText)findViewById(R.id.TFUname);
-            EditText p = (EditText)findViewById(R.id.TFPword);
-            Intent i = new Intent(MainActivity.this, Display.class);
-
-            String uname = a.getText().toString();
-            String pword = p.getText().toString();
-
-            boolean b = Pattern.matches("[a-zA-Z0-9]{4,16}", uname);
-            if(b == true)
-            {
-
-                i.putExtra("Username",uname);
-                startActivity(i);
-            }
-            else
-            {
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setMessage(" Username or Password Must Be :" +  "\n" +
-                        " ALPHANUMERIC" + "\n" +
-                        " 8 to 24 Characters");
-                builder.setPositiveButton("Okay", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.cancel();
-
-                    }
-                });
-                AlertDialog alert = builder.create();
-                alert.show();
-
-            }
-        }
-    }
-
-    public void clickExit(View x)
-    {
-        finish();
-    }
-
 }
 
 
