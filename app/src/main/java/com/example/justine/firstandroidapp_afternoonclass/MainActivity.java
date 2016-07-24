6e7b066ecb5d1ca422057cc8b0bc2e977bb22702
Justine <justine.jay.fernandez@adamson.edu.ph>
2016-07-24 21:22:03 +0800

    With Image and other files

---
+++ app/src/main/java/com/example/justine/firstandroidapp_afternoonclass/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.justine.firstandroidapp_afternoonclass;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Toast;
+import java.util.regex.Pattern;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final EditText email = (EditText)findViewById(R.id.email);
+        final EditText password = (EditText)findViewById(R.id.password);
+        Button login = (Button)findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (email != null && password != null) {
+                    if (Pattern.compile("([a-zA-Z0-9]+_?)+@[a-zA-Z0-9]+\\.com").matcher(email.getText()).matches()) {
+                            if (password.length() > 8) {
+                                Toast.makeText(getApplicationContext(), "Redirecting...", Toast.LENGTH_SHORT).show();
+                                Intent a = new Intent(MainActivity.this, welcome.class);
+                                startActivity(a);
+                                finish();
+                            } else {
+                                Toast.makeText(getBaseContext(), "Password too short", Toast.LENGTH_SHORT).show();
+                            }
+                    } else {
+                        Toast.makeText(getBaseContext(),"Invalid Email address", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "Please fill in the blank field",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+    }
+}
