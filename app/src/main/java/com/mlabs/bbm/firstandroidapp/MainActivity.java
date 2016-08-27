7e82bf014db33fae6291035c4ad68604fe08b19b
veeldyow <veeldyow@yahoo.com>
2016-08-27 19:45:24 +0800

    exit button and counter removed

--- app/src/main/java/com/mlabs/bbm/firstandroidapp/MainActivity.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp/MainActivity.java
@@ -14,86 +14,65 @@ import android.widget.Toast;
 import android.text.InputType;
 
 public class MainActivity extends AppCompatActivity {
-    Button sign,exit;
-    EditText etext,ptext;
-    TextView errormsg,tv7;
-        int ctr = 3;
+    Button sign, exit;
+    EditText etext, ptext;
+    TextView tv7;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        sign=(Button)findViewById(R.id.button);
-        exit=(Button)findViewById(R.id.button2);
-        etext=(EditText)findViewById(R.id.editText);
-        ptext=(EditText)findViewById(R.id.editText2);
-
-        errormsg=(TextView)findViewById(R.id.textView3);
-        errormsg.setVisibility(View.GONE);
-        //show=(TextView)findViewById(R.id.show);
-        tv7=(TextView)findViewById(R.id.textView7);
+        sign = (Button) findViewById(R.id.button);
+        etext = (EditText) findViewById(R.id.editText);
+        ptext = (EditText) findViewById(R.id.editText2);
+        tv7 = (TextView) findViewById(R.id.textView7);
 
         tv7.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent event) {
-                switch (event.getAction() ) {
-                    case MotionEvent.ACTION_DOWN:
-                        ptext.setTransformationMethod(null);
-                        break;
-                    case MotionEvent.ACTION_UP:
-                        ptext.setTransformationMethod(new PasswordTransformationMethod());
-                        break;
-                }
-                return true;
-            }
-        }
+                                   @Override
+                                   public boolean onTouch(View view, MotionEvent event) {
+                                       switch (event.getAction()) {
+                                           case MotionEvent.ACTION_DOWN:
+                                               ptext.setTransformationMethod(null);
+                                               break;
+                                           case MotionEvent.ACTION_UP:
+                                               ptext.setTransformationMethod(new PasswordTransformationMethod());
+                                               break;
+                                       }
+                                       return true;
+                                   }
+                               }
         );
-        sign.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                if (etext.getText().toString().equals("veeldyow@yahoo.com") && ptext.getText().toString().equals("catiis")) {
-                    Toast.makeText(getApplicationContext(), "Redirecting....", Toast.LENGTH_SHORT).show();
+        sign.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        if (etext.getText().toString().equals("veeldyow@yahoo.com") && ptext.getText().toString().equals("catiis")) {
+                                            Toast.makeText(getApplicationContext(), "Redirecting....", Toast.LENGTH_SHORT).show();
 
-                    Thread timerThread = new Thread(){
-                        public void run(){
-                            try{
-                                sleep(1000);
-                            }
-                            catch(InterruptedException e){
-                                e.printStackTrace();
-                            }
-                            finally{
-                                Intent nextmo = new Intent(MainActivity.this,WelcomeScreen.class );
-                                startActivity(nextmo);
-                            }
-                        }
-                    };
-                    timerThread.start();
-                }
-                else {
-                    Toast.makeText(getApplicationContext(), "Incorrect Email/Password", Toast.LENGTH_SHORT).show();
-                    errormsg.setVisibility(View.VISIBLE);
-                    errormsg.setBackgroundColor(Color.RED);
-                    ctr--;
-                    errormsg.setText(Integer.toString(ctr));
+                                            Thread timerThread = new Thread() {
+                                                public void run() {
+                                                    try {
+                                                        sleep(1000);
+                                                    } catch (InterruptedException e) {
+                                                        e.printStackTrace();
+                                                    } finally {
+                                                        Intent nextmo = new Intent(MainActivity.this, WelcomeScreen.class);
+                                                        startActivity(nextmo);
+                                                    }
+                                                }
+                                            };
+                                            timerThread.start();
+                                        } else {
+                                            Toast.makeText(getApplicationContext(), "Incorrect Email/Password", Toast.LENGTH_SHORT).show();
 
-                    if (ctr == 0) {
-                        sign.setEnabled(false);
-                    }
-                }
-            }
-        });
-
-        exit.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                finish();
-            }
-        });
-
-
+                                        }
+                                    }
+                                }
+        );
     }
-
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
 }
