f82e7f0eaabcc187438d971a7689472f096396b1
DTSMI <jason.halili@yahoo.com>
2016-08-27 18:54:41 +0800

    Please let this function work for the meantime

--- app/src/main/java/com/mlabs/bbm/firstandroidapp/LogInActivity.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp/LogInActivity.java
@@ -3,6 +3,8 @@ package com.mlabs.bbm.firstandroidapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.PasswordTransformationMethod;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,6 +22,7 @@ public class LogInActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.log_in_activity);
         Button button1 = (Button) findViewById(R.id.button1);
+        Button show = (Button) findViewById(R.id.button);
 
         final EditText editemail = (EditText) findViewById(R.id.editText);
         final EditText editpass = (EditText) findViewById(R.id.editText2);
@@ -30,13 +33,42 @@ public class LogInActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String email = editemail.getText().toString();
                 String passa = editpass.getText().toString();
-                if((!isValidEmail(email))||(!isValidPassword(passa))){
+                if ((!isValidEmail(email)) || (!isValidPassword(passa))) {
                     Toast.makeText(LogInActivity.this, "Incorrect Username or Password", Toast.LENGTH_SHORT).show();
-                }
-                else{
+                } else {
                     Intent intent = new Intent(v.getContext(), MainActivity.class);
-                    startActivityForResult(intent,0);
+                    startActivityForResult(intent, 0);
                 }
+            }
+        });
+
+
+        show.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                int eventA = motionEvent.getAction();
+
+/*                if (eventA == motionEvent.ACTION_UP) {
+                    editpass.setTransformationMethod(new PasswordTransformationMethod());
+                } else if (eventA == motionEvent.ACTION_CANCEL) {
+                    editpass.setTransformationMethod(new PasswordTransformationMethod());
+                } else if (eventA == motionEvent.ACTION_DOWN) {
+                    editpass.setTransformationMethod(null);
+                }
+                return true; */
+
+                switch (eventA) {
+                    case MotionEvent.ACTION_DOWN:
+                        editpass.setTransformationMethod(null);
+                          break;
+                    case MotionEvent.ACTION_UP:
+                        editpass.setTransformationMethod(new PasswordTransformationMethod());
+                        break;
+                    case MotionEvent.ACTION_CANCEL:
+                        editpass.setTransformationMethod(new PasswordTransformationMethod());
+                        break;
+                }
+                return true;
             }
         });
     }
@@ -55,4 +87,5 @@ public class LogInActivity extends AppCompatActivity {
         }
         return false;
     }
+
 }
