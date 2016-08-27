52dd47717f25447023c8be88db457c101d45d436
Yvette Kiak <yvetteprivate@gmail.com>
2016-08-27 18:47:19 +0800

    With Show Password

--- app/src/main/java/com/mlabs/bbm/firstandroidapp/Login.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp/Login.java
@@ -6,8 +6,10 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.PatternMatcher;
+import android.text.InputType;
 import android.util.Log;
 import android.util.Patterns;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -29,24 +31,48 @@ public class Login extends AppCompatActivity {
         editText1=(EditText) findViewById(R.id.usertxt);
         editText2=(EditText) findViewById(R.id.passwordtxt);
         Button=(Button) findViewById(R.id.Buttonlog);
+
+        Button show = (Button) findViewById(R.id.show);
+
+
         Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (editText1.getText() == null || !Patterns.EMAIL_ADDRESS.matcher(editText1.getText()).matches()) {
                     Toast.makeText(getBaseContext(), "Invalid Email", Toast.LENGTH_SHORT).show();
                 } else {
-                    if (editText2.getText().length() == 6) {
+                    if (editText2.getText().length() >= 6) {
                         Intent intent = new Intent(Login.this, MainActivity.class);
                         startActivity(intent);
+                        finish();
                     } else {
-                        Toast.makeText(getBaseContext(), "Need 6 Characters", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getBaseContext(), "Need six or more Characters", Toast.LENGTH_SHORT).show();
                     }
                 }
             }
         });
 
+    show.setOnTouchListener(new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent)
+        {
+
+            switch ( motionEvent.getAction() ) {
+                case MotionEvent.ACTION_DOWN:
+                    editText2.setInputType(InputType.TYPE_CLASS_TEXT);
+                    break;
+                case MotionEvent.ACTION_UP:
+                    editText2.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    break;
+            }
+            return true;
+        }
+        });
+
+
+        }
 
 
     }
-}
+
 
