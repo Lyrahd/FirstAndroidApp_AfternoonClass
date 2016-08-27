a02f93ea2f082a3c2b93790c54917972455334a7
AnicieteJ <androidstudio@cl7-13.local>
2016-08-27 19:25:33 +0800

    Show Password

--- app/src/main/java/com/mlabs/bbm/firstandroidapp/MainActivity.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AlertDialog;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.MotionEvent;
 import android.view.View;
 import  android.content.Intent;
 import android.widget.Button;
@@ -20,6 +22,8 @@ public class MainActivity extends ActionBarActivity {
     EditText editText_username;
     EditText editText_password;
     Button button_submit;
+    Button button_show;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +32,8 @@ public class MainActivity extends ActionBarActivity {
         editText_username = (EditText) findViewById(R.id.editText_user);
         editText_password = (EditText) findViewById(R.id.editText_password);
         button_submit = (Button) findViewById(R.id.button_submit);
+        button_show = (Button) findViewById(R.id.button_show);
+
 
         button_submit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,6 +66,24 @@ public class MainActivity extends ActionBarActivity {
                 return matcher.matches();
             }
         });
+
+        button_show.setOnTouchListener(new View.OnTouchListener() {
+                        public boolean onTouch(View v, MotionEvent show) {
+                                if (show.getAction() == MotionEvent.ACTION_DOWN){
+                                        editText_password.setInputType(InputType.TYPE_CLASS_TEXT);
+                                    }
+                                else if (show.getAction()==MotionEvent.ACTION_UP){
+                                        editText_password.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                                    }
+                                else if (show.getAction()==MotionEvent.ACTION_CANCEL){
+                                    editText_password.setInputType(InputType.TYPE_CLASS_TEXT)                             ;
+                                        }
+                                return true;
+                            }
+                    });
+
+        
+
     }
 
 }
