77bb2f35e68aa1e37734f1e4ee272bbe4a15f7da
Keifer Fababeir <kffababeir@gmail.com>
2016-08-27 18:06:54 +0800

    implement view password ontouch 1

--- app/src/main/java/com/mlabs/bbm/firstandroidapp/MainActivity.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
+import android.text.method.PasswordTransformationMethod;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
@@ -35,10 +36,11 @@ public class MainActivity extends AppCompatActivity {
             public boolean onTouch(View view, MotionEvent event) {
                 switch (event.getAction() ) {
                     case MotionEvent.ACTION_DOWN:
-                        //pwd.setTransformationMethod(null);
-                        pwd.setInputType(InputType.TYPE_CLASS_TEXT);
+                        pwd.setTransformationMethod(null);
+                        //pwd.setInputType(InputType.TYPE_CLASS_TEXT);
                         break;
                     case MotionEvent.ACTION_UP:
+                        pwd.setTransformationMethod(new PasswordTransformationMethod());
                         //pwd.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
                         break;
                 }
