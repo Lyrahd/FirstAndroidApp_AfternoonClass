ca2023c4d93a3c6f5786d8a3b802309276876273
Mary Ann Jane Antonio <mary.ann.jane.antonio@adamson.edu.ph>
2016-08-27 19:42:26 +0800

    on touch listener

--- app/src/main/java/com/mlabs/bbm/firstandroidapp/MainActivity.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp/MainActivity.java
@@ -1,19 +1,22 @@
 package com.mlabs.bbm.firstandroidapp;
 
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.PasswordTransformationMethod;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import android.content.Context;
-
+import android.view.View.OnTouchListener;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn1;
+    Button btn1, show;
     EditText etxt1,etxt2;
 
 
@@ -26,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         btn1=(Button)findViewById(R.id.login);
         etxt1=(EditText)findViewById(R.id.email_address);
         etxt2=(EditText)findViewById(R.id.pwd);
+        show=(Button)findViewById(R.id.show);
         final Context context = this;
 
 
@@ -33,11 +37,16 @@ public class MainActivity extends AppCompatActivity {
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(etxt1.getText().toString().equals("admin") && etxt2.getText().toString().equals("admin"))
+                if(etxt1.getText().toString().equals("antoniomaryannjane@yahoo.com") && etxt2.getText().toString().equals("admin"))
                 {
+                    //Toast.makeText(getApplicationContext(), "Loading......",Toast.LENGTH_SHORT).show();
 
                     Intent intent = new Intent(context, MainForm.class);
-                    startActivity(intent);   }
+                    startActivity(intent);
+                    etxt1.setText("");
+                    etxt2.setText("");
+
+                }
 
                 else
                 {
@@ -46,8 +55,29 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-    }
+        show.setOnTouchListener(new OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                
+                switch ( event.getAction() ) {
+                    case MotionEvent.ACTION_DOWN:
+                        etxt2.setTransformationMethod(null);
+                     break;
 
-}
+                    case MotionEvent.ACTION_UP:
+                        etxt2.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                     break;
+
+                    //case MotionEvent.ACTION_CANCEL:
+                        //etxt2.setTransformationMethod(null);
+                        //return true;
+                }
+                return true;}
+        });
+
+
+
+
+}}
 
 
