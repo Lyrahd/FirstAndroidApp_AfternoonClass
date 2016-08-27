3237761804cc2145ed8d915c0f46db7baa6e73c7
veeldyow <veeldyow@yahoo.com>
2016-08-27 19:26:08 +0800

    show password

--- app/src/main/java/com/mlabs/bbm/firstandroidapp/MainActivity.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp/MainActivity.java
@@ -4,20 +4,22 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import android.text.InputType;
 
 public class MainActivity extends AppCompatActivity {
     Button sign,exit;
     EditText etext,ptext;
-    TextView eview,pview,errormsg;
-    ImageView logo;
-    int ctr = 3;
+    TextView errormsg,tv7;
+        int ctr = 3;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,9 +32,25 @@ public class MainActivity extends AppCompatActivity {
 
         errormsg=(TextView)findViewById(R.id.textView3);
         errormsg.setVisibility(View.GONE);
+        //show=(TextView)findViewById(R.id.show);
+        tv7=(TextView)findViewById(R.id.textView7);
 
+        tv7.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                switch (event.getAction() ) {
+                    case MotionEvent.ACTION_DOWN:
+                        ptext.setTransformationMethod(null);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        ptext.setTransformationMethod(new PasswordTransformationMethod());
+                        break;
+                }
+                return true;
+            }
+        }
+        );
         sign.setOnClickListener(new View.OnClickListener(){
-
             @Override
             public void onClick(View v) {
                 if (etext.getText().toString().equals("veeldyow@yahoo.com") && ptext.getText().toString().equals("catiis")) {
@@ -77,4 +95,5 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
 }
