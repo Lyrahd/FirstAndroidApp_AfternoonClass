8354cc713957366266ea40ad453c3567f2e30862
Timothy Calilao <Calilao20@gmail.com>
2016-08-27 19:08:42 +0800

    Edited ACTiON_CANCEL

--- app/src/main/java/com/mlabs/bbm/firstandroidapp/login.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp/login.java
@@ -64,7 +64,7 @@ public class login extends AppCompatActivity {
                         txtPass.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
                         break;
                     case MotionEvent.ACTION_CANCEL:
-                        txtPass.setInputType(InputType.TYPE_CLASS_TEXT);
+                    txtPass.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
                         break;
                 }
                  **/
@@ -75,7 +75,7 @@ public class login extends AppCompatActivity {
                     txtPass.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
                 }
                 else if (event.getAction()==MotionEvent.ACTION_CANCEL){
-                    txtPass.setInputType(InputType.TYPE_CLASS_TEXT);
+                    txtPass.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
                 }
                 return true;
             }
