db6e10a242344a26edd60946fd5daa00efa78fa8
thonbarroro <superman09>
2016-08-27 19:14:30 +0800

    if else and switch

--- app/src/main/java/com/mlabs/bbm/firstandroidapp/MainActivity.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp/MainActivity.java
@@ -5,6 +5,9 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     EditText password;
     Button login_button;
     int attempt_counter = 3;
+    Button show;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         username = (EditText) findViewById(R.id.email_address);
         password = (EditText) findViewById(R.id.pwd);
         login_button = (Button) findViewById(R.id.btn);
+        show =(Button)findViewById(R.id.btnShow);
 
         attempt_counter = attempt_counter + 1;
 
@@ -62,5 +67,30 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+        show.setOnTouchListener(new View.OnTouchListener() {
+                                    @Override
+                                    public boolean onTouch(View view, MotionEvent motionEvent) {
+                                        int event = motionEvent.getAction();
+
+                                       /* if (event == motionEvent.ACTION_DOWN) {
+                                            password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                                        } else if (event ==motionEvent.ACTION_UP){
+                                            password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                                        }
+                                        return false;
+                                        */
+                                        switch(event){
+                                            case MotionEvent.ACTION_DOWN:password.setTransformationMethod(null);
+                                                break;
+                                            case MotionEvent.ACTION_UP: password.setTransformationMethod(new PasswordTransformationMethod());
+                                                break;
+                                            case MotionEvent.ACTION_CANCEL: password.setTransformationMethod(new PasswordTransformationMethod());
+                                        }
+                                        return false;
+                                    }
+                                }
+
+        );
+
     }
 }
