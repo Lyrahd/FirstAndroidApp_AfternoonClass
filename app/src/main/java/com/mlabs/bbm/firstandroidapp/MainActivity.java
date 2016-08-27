219a70084d465dfd3dff199d00b02c9a8e60b848
Rafael Balboa <rafaelbalboa25@yahoo.com>
2016-08-27 19:00:21 +0800

    OnTouchListener

--- app/src/main/java/com/mlabs/bbm/firstandroidapp/MainActivity.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp/MainActivity.java
@@ -3,23 +3,30 @@ package com.mlabs.bbm.firstandroidapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import android.view.View.OnTouchListener;
 
 public class MainActivity extends AppCompatActivity {
+
+Button showbtn, validate;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        showbtn = (Button) findViewById(R.id.show);
         setContentView(R.layout.activity_main);
         final EditText email = (EditText) findViewById(R.id.email);
         final EditText password = (EditText) findViewById(R.id.pass);
-        Button validate = (Button) findViewById(R.id.buttonL);
+        validate = (Button) findViewById(R.id.buttonL);
         validate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -34,11 +41,45 @@ public class MainActivity extends AppCompatActivity {
                             Toast.LENGTH_SHORT);
                     toast.setGravity(Gravity.TOP | Gravity.LEFT, 0, 0);
                     toast.show();
-                    Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+                    Intent intent = new Intent(MainActivity.this, Main2Activity.class);
                     startActivity(intent);
                 }
             }
-        });}
+        });
+
+        showbtn.setOnTouchListener(new OnTouchListener() {
+            @Override
+            public boolean onTouch(View view,MotionEvent motionEvent){
+            int event = motionEvent.getAction();
+     //       if (motionEvent.getAction() == MotionEvent.ACTION_DOWN)
+   //         {
+ //               password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+
+            //}else if (motionEvent.getAction() == MotionEvent.ACTION_UP)
+           // {
+             //   password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+            //}
+           // return false;
+            switch (event)
+            {
+                case MotionEvent.ACTION_DOWN:
+                    password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                    break;
+                case MotionEvent.ACTION_UP:
+                    password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    break;
+            }
+            return true;
+
+
+
+        }
+        });
+    }
+
+
+
+
     private boolean validateEmail(String email) {
         String emailRegEx;
         Pattern pattern;
@@ -58,6 +99,13 @@ public class MainActivity extends AppCompatActivity {
         } else
             return false;
     }
+
+
+
+
+
+
+
 }
 
 
