135016eae5ed8b48adb397d39ea5db293d6da4bd
Alwyn Villanueva <alwyn.villanueva@adamson.edu.ph>
2016-08-27 18:48:13 +0800

    added ontouchevent

--- app/src/main/java/com/mlabs/bbm/firstandroidapp/MainActivity.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp/MainActivity.java
@@ -5,6 +5,9 @@ import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.text.method.PasswordTransformationMethod;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,10 +23,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Button btn;
         final EditText emailAdd,passWord;
-
+        Button show;
         emailAdd=(EditText)findViewById(R.id.editText);
         passWord=(EditText)findViewById(R.id.editText2);
         btn = (Button)findViewById(R.id.button);
+        show = (Button)findViewById(R.id.show);
         if (btn!=null){
             btn.setOnClickListener(new View.OnClickListener(){
                 @Override
@@ -49,6 +53,22 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
         }
+        show.setOnTouchListener(new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View v, MotionEvent e) {
+            switch (e.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    passWord.setTransformationMethod(null);
+                    return true;
+                case MotionEvent.ACTION_CANCEL:
+                case MotionEvent.ACTION_UP:
+                    passWord.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                return true;
+            }
+            return  false;
+        }
+
+    });
     }
 
 
