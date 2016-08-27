2f0ac49f0fb52562a1f894252438f963735ba36e
Justine Fernandez <justine.jay.fernandez@adamson.edu.ph>
2016-08-27 18:52:15 +0800

    OnTouchListener

--- app/src/main/java/com/example/justine/firstandroidapp_afternoonclass/MainActivity.java
+++ app/src/main/java/com/example/justine/firstandroidapp_afternoonclass/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.justine.firstandroidapp_afternoonclass;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.widget.EditText;
 import android.widget.Button;
 import android.content.Intent;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Toast;
 import java.util.regex.Pattern;
@@ -15,31 +17,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final EditText email = (EditText)findViewById(R.id.email);
-        final EditText password = (EditText)findViewById(R.id.password);
-        Button login = (Button)findViewById(R.id.login);
+        final EditText email = (EditText) findViewById(R.id.email);
+        final EditText password = (EditText) findViewById(R.id.password);
+        Button show = (Button) findViewById(R.id.show);
+        // Button for Show
+        show.setOnTouchListener(new View.OnTouchListener(){
+            @Override
+            public boolean onTouch(View view, MotionEvent motion){
+                switch ( motion.getAction() ) {
+                    case MotionEvent.ACTION_DOWN:
+                        password.setInputType(InputType.TYPE_CLASS_TEXT);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        password.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                        break;
+                }
+                return true;
+            }
+        });
+        // Button for Login
+        Button login = (Button) findViewById(R.id.login);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (email != null && password != null) {
                     if (Pattern.compile("([a-zA-Z0-9]+_?)+@[a-zA-Z0-9]+\\.com").matcher(email.getText()).matches()) {
-                            if (password.length() > 8) {
-                                Toast.makeText(getApplicationContext(), "Redirecting...", Toast.LENGTH_SHORT).show();
-                                Intent a = new Intent(MainActivity.this, welcome.class);
-                                startActivity(a);
-                                finish();
-                            } else {
-                                Toast.makeText(getBaseContext(), "Password too short", Toast.LENGTH_SHORT).show();
-                            }
+                        if (password.length() > 8) {
+                            Toast.makeText(getApplicationContext(), "Redirecting...", Toast.LENGTH_SHORT).show();
+                            Intent a = new Intent(MainActivity.this, welcome.class);
+                            startActivity(a);
+                            finish();
+                        } else {
+                            Toast.makeText(getBaseContext(), "Password too short", Toast.LENGTH_SHORT).show();
+                        }
                     } else {
-                        Toast.makeText(getBaseContext(),"Invalid Email address", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getBaseContext(), "Invalid Email address", Toast.LENGTH_SHORT).show();
                     }
-                }
-                else{
-                    Toast.makeText(getApplicationContext(), "Please fill in the blank field",Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Please fill in the blank field", Toast.LENGTH_SHORT).show();
                 }
 
             }
         });
+
     }
 }
