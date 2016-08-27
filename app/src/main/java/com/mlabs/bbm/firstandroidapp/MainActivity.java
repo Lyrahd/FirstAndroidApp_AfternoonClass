6d439d6411c8caabc248d958bb4b3cef0c280f23
Christian Valondo <christian_valondo@yahoo.com>
2016-08-27 19:06:09 +0800

    Password with show button

--- app/src/main/java/com/mlabs/bbm/firstandroidapp/MainActivity.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp/MainActivity.java
@@ -3,10 +3,13 @@ package com.mlabs.bbm.firstandroidapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.text.InputType;
 import android.text.TextUtils;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
 
 
 public class MainActivity extends AppCompatActivity {
@@ -24,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
 
     Button btn1;
     EditText edtxt1,edtxt2;
+    Button show;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -33,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
         btn1=(Button)findViewById(R.id.login_button);
         edtxt1=(EditText)findViewById(R.id.email_textbox);
         edtxt2=(EditText)findViewById(R.id.password_textbox);
-
+        show = (Button)findViewById(R.id.show);
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -73,6 +78,23 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        show.setOnTouchListener(new View.OnTouchListener(){
+                @Override
+            public boolean onTouch(View v, MotionEvent event){
+                switch (event.getAction()) {
+
+                    case MotionEvent.ACTION_DOWN:
+
+                        edtxt2.setInputType(InputType.TYPE_CLASS_TEXT);
+                        break;
+                    case MotionEvent.ACTION_UP:
+
+                        edtxt2.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                        break;
+                }
+                return true;
+            }
+        });
 
     }
 
