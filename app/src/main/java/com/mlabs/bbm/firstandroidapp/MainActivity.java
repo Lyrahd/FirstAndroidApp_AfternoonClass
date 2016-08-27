2f2ef2cc5460882098eb342f7112c0e91c87b13f
Mari May Jorda <androidstudio@cl7-25.local>
2016-08-27 19:02:41 +0800

    OnTouchListener

--- app/src/main/java/com/mlabs/bbm/firstandroidapp/MainActivity.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp/MainActivity.java
@@ -3,7 +3,10 @@ package com.mlabs.bbm.firstandroidapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,6 +16,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
+    Button show;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,25 +25,59 @@ public class MainActivity extends AppCompatActivity {
         final EditText username = (EditText) findViewById(R.id.txt_un);
         final EditText password = (EditText) findViewById(R.id.txt_pw);
         Button validate = (Button) findViewById(R.id.btn_login);
+        show = (Button) findViewById(R.id.show);
 
         assert validate != null;
-        validate.setOnClickListener(new View.OnClickListener(){
+        validate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(!validateEmail(username.getText().toString())){
+                if (!validateEmail(username.getText().toString())) {
                     username.setError("Invalid Email");
                     username.requestFocus();
-                }else if(!validatePassword(password.getText().toString())){
+                } else if (!validatePassword(password.getText().toString())) {
                     password.setError("Invalid Password");
                     password.requestFocus();
-                }else{
+                } else {
                     Toast toast = Toast.makeText(getApplicationContext(), "Your toast message.",
                             Toast.LENGTH_SHORT);
                     toast.setGravity(Gravity.CENTER, 0, 0);
                     toast.show();
-                    Intent intent = new Intent(MainActivity.this,SplashScreen .class );
+                    Intent intent = new Intent(MainActivity.this, WelcomeScreen.class);
                     startActivity(intent);
+                    finish();
                 }
+            }
+
+        });
+
+        //ShowPassword
+        show.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+
+                int event = motionEvent.getAction();
+
+                switch (motionEvent.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                        return true;
+                    case MotionEvent.ACTION_UP:
+                        password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                        return true;
+                    case MotionEvent.ACTION_CANCEL:
+                        password.setTransformationMethod(new PasswordTransformationMethod());
+                        return true;
+                }
+
+                /*if (event == MotionEvent.ACTION_DOWN) {
+                    password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                } else if (event == MotionEvent.ACTION_UP) {
+                    password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                } else if (event == MotionEvent.ACTION_CANCEL) {
+                    password.setTransformationMethod(null);
+                }*/
+
+                return true;
             }
 
         });
