528ad8b52a2ffeb996dcaa887d98a4a8d1761227
acmontinola <anjela.camille.montinola@adamson.edu.ph>
2016-09-10 00:32:44 +0800

    Merge branch 'montinolaAC' of https://github.com/MobileComputingClass/FirstAndroidApp_AfternoonClass into montinolaAC
    # Conflicts:
    #	app/src/main/java/com/mlabs/bbm/firstandroidapp/MainActivity.java
    #	app/src/main/java/com/mlabs/bbm/firstandroidapp/SplashScreen.java
    #	app/src/main/res/drawable/splash_image.jpg
    #	app/src/main/res/layout/activity_main.xml
    #	app/src/main/res/layout/splash.xml
    #	app/src/main/res/values/styles.xml

--- app/src/main/java/com/mlabs/bbm/firstandroidapp/WelcomeActivity.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp/WelcomeActivity.java
@@ -1,15 +1,23 @@
 package com.mlabs.bbm.firstandroidapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-/**
- * Created by MONTNOLA on 7/22/2016.
- */
 public class WelcomeActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        Button back1 = (Button) findViewById(R.id.btn_ClickMe);
+        back1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WelcomeActivity.this,ClickMeActivity.class );
+                startActivity(intent);
+            }
+        });
     }
 }
