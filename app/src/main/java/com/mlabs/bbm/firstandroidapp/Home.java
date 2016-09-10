b05d26f036bff69d34a57bd249d1cbc7bebb66fa
Fatima Ybanez <fatima.ybanez_06@yahoo.com>
2016-09-10 01:41:46 -1200

    On Touch Listener (Identifying x and y axis)

--- app/src/main/java/com/mlabs/bbm/firstandroidapp/Home.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp/Home.java
@@ -1,8 +1,9 @@
 package com.mlabs.bbm.firstandroidapp;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-
+import android.view.View;
+import android.widget.Button;
 
 
 public class Home extends android.app.Activity {
@@ -13,6 +14,16 @@ public class Home extends android.app.Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homel);
 
+        Button next= (Button) findViewById(R.id.btnMain);
 
-    }
-}
+        assert next != null;
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Home.this,Main .class );
+                startActivity(intent);
+            }
+
+            });
+        }}
--- app/src/main/java/com/mlabs/bbm/firstandroidapp/SplashScreen.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp/SplashScreen.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
-
 public class SplashScreen extends Activity{
 
     @Override
