6ad6db16da82d19cb2bc8b84bfabd3162963674c
Angela Margallo <anglmrgll0213@gmail.com>
2016-09-17 19:07:17 +0800

    layout

---
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp/DatabaseAdapter.java
@@ -0,0 +1,17 @@
+package com.mlabs.bbm.firstandroidapp;
+
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by androidstudio on 17/09/16.
+ */
+public class DatabaseAdapter extends SQLiteOpenHelper {
+    private static final String TAG = DatabaseAdapter.class.getSimpleName();
+    private static final String DATABASE_NAME="users.db";
+    private static final int DATABASE_VERSION= 1;
+    private static final String TABLE_USER="user";
+    private static final String KEY_ID="id";
+    private static final String KEY_EMAIL
+
+
+}
