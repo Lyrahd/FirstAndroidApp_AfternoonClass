238738f9ce7d4f78080b9b74fb9de2057a192a05
Elmer Tanghal <johnelmerm.tanghal@yahoo.com>
2016-09-17 19:16:07 +0800

    Edited Validations

--- app/src/main/java/com/mlabs/bbm/firstandroidapp/signup.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp/signup.java
@@ -12,6 +12,48 @@ public class signup extends AppCompatActivity{
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
+        final EditText email = (EditText)findViewById(R.id.editText3);
+        final EditText pwd = (EditText)findViewById(R.id.editText4);
+        final EditText cpwd = (EditText)findViewById(R.id.editText5);
+        final Button btnSU = (Button)findViewById(R.id.button3);
+
+        final String emailInput = email.getText().toString().trim();
+        final String passwordInput = pwd.getText().toString().trim();
+        final String passwordInputVerify = cpwd.getText().toString().trim();
+        if (btnSU!=null){
+            btnSU.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public  void onClick(View v) {
+                if (!emailInput.isEmpty() && !passwordInput.isEmpty() && !passwordInputVerify.isEmpty()) {
+                    if(Pattern.compile("[a-zA-Z0-9]+_?+@[a-zA-Z0-9]+\\.com").matcher(emailInput).matches()){
+                        }
+                        else
+                            Toast.makeText(getBaseContext(), "Invalid Email Address", Toast.LENGTH_SHORT).show();
+                    if (!(passwordInput.length() == 0)) {
+                        if (passwordInput.length() > 8) {
+                            Intent intent = new Intent(signup.this, blankAct.class);
+                            startActivity(intent);
+                        } else
+                            Toast.makeText(getBaseContext(), "Password too short", Toast.LENGTH_SHORT).show();
+
+                        if (passwordInput.equals(passwordInputVerify)) {
+                            sqlDB.registerUser(emailInput, passwordInput, getCurrentDateTime());
+                            Toast.makeText(getApplicationContext(), "User Successfully aded", Toast.LENGTH_LONG).show();
+                            Intent goBackToMainActivity = new Intent(getApplicationContext(), MainActivity.class);
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Password did not match", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+                    else{
+                    Toast.makeText(getApplicationContext(), "Please fillup required fields", Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+                
+            });
+
+        }
     }
 
 }
