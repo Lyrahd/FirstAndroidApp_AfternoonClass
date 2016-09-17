4bb54ed358df4ead214d762e475a4ad4107e7509
veeldyow <veeldyow@yahoo.com>
2016-09-17 18:46:58 +0800

    implement layout 2

---
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp/Register.java
@@ -0,0 +1,16 @@
+package com.mlabs.bbm.firstandroidapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by androidstudio on 17/09/16.
+ */
+public class Register extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+    }
+
+}
