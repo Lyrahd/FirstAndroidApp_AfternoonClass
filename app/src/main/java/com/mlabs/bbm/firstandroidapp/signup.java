a64508ba87963e9dd8a74a0b86ebfe0c0b0ae5a1
Alwyn Villanueva <alwyn.villanueva@adamson.edu.ph>
2016-09-17 17:42:19 +0800

    SQLlite finals Act

---
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp/signup.java
@@ -0,0 +1,17 @@
+package com.mlabs.bbm.firstandroidapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by androidstudio on 17/09/16.
+ */
+public class signup extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup);
+    }
+
+}
