7cb6a56adb8d65d4e478836bafc45b34618a2210
Alexandra Andallon <alexandra.andallon@adamson.edu.ph>
2016-09-17 17:11:58 +0800

    onTouch Activity

--- app/src/main/java/com/mlabs/bbm/firstandroidapp/MainActivity.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp/MainActivity.java
@@ -1,17 +1,31 @@
 package com.mlabs.bbm.firstandroidapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
-
-
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
+
+    Button onTouchbtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        onTouchbtn=(Button) findViewById(R.id.onTouchbtn);
+
+    }
+
+    public void onTouch(View view) {
+
+        Intent intent = new Intent(MainActivity.this, OnTouchActivity.class);
+        startActivity(intent);
+
+
     }
 }
 
