864b8bab305ffef081523c6cca06b63e3b32c497
Angela Margallo <anglmrgll0213@gmail.com>
2016-09-21 14:00:51 +0800

    with Database Adapter and Validation

--- app/src/main/java/com/mlabs/bbm/firstandroidapp/MainActivity.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp/MainActivity.java
@@ -10,10 +10,13 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    LoginDataBaseAdapter loginDataBaseAdapter;
     public static boolean Email_Validate(CharSequence email) {
         if (TextUtils.isEmpty(email)) {
             return false;
@@ -22,20 +25,25 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private Toast popToast;
     Button login_btn,show_btn;
     EditText email_txt,password_txt;
-    TextView signup;
-    
+    TextView txtsign;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        signup=(TextView)findViewById(R.id.txtsignup);
         login_btn=(Button)findViewById(R.id.login_btn);
         email_txt=(EditText)findViewById(R.id.email_txt);
         password_txt=(EditText)findViewById(R.id.password_txt);
         show_btn=(Button)findViewById(R.id.show_btn);
+        txtsign = (TextView) findViewById(R.id.txtsignup);
+        // create a instance of SQLite Database
+        loginDataBaseAdapter = new LoginDataBaseAdapter(this);
+        loginDataBaseAdapter = loginDataBaseAdapter.open();
+
+        popToast = Toast.makeText(getApplicationContext(), null, Toast.LENGTH_SHORT);
 
 
         login_btn.setOnClickListener(new View.OnClickListener() {
@@ -48,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
                 if(Email_Validate(email_txt.getText())==true && password_txt.getText().length()>0)
                 {
                     Intent i;
-                    i = new Intent(MainActivity.this, Activity.class);
+                    i = new Intent(MainActivity.this, TouchListener.class);
                     startActivity(i);
                     finish();
                     //hello
@@ -76,39 +84,66 @@ public class MainActivity extends AppCompatActivity {
                     password_txt.setError(getString(R.string.error_field_required));
                 }
 
+                else {
+                    String userName = email_txt.getText().toString();
+                    String password = password_txt.getText().toString();
+
+                    // fetch the Password form database for respective user name
+                    String storedPassword = loginDataBaseAdapter.getSinlgeEntry(userName);
+
+                    // check if the Stored password matches with  Password entered by user
+                    if (password.equals(storedPassword)) {
+                        Toast.makeText(MainActivity.this, userName + " has logged in. \n Password: " + password, Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent(MainActivity.this, TouchListener.class);
+                        startActivity(intent);
+                    //dialog.dismiss();
+                    } else {
+                        Toast.makeText(MainActivity.this, "User Name or Password is incorrect", Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                        startActivity(intent);
+
+                    }
+                }
+
             }
+
         });
-        
-        signup.setOnClickListener(new View.OnClickListener(){
+
+        //txtREg keme
+        txtsign.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v){
-                Intent intent = new Intent(MainActivity.this, SignUp.class);
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(MainActivity.this,SignUp.class );
                 startActivity(intent);
             }
+
         });
 
-    show_btn.setOnTouchListener(new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            int event = motionEvent.getAction();
-            final int cursor=password_txt.getSelectionStart();
 
-            switch (event) {
-                case MotionEvent.ACTION_DOWN:
-                    password_txt.setTransformationMethod(null);
-                    password_txt.setSelection(cursor);
-                    return true;
-                case MotionEvent.ACTION_CANCEL:
-                    password_txt.setTransformationMethod(PasswordTransformationMethod.getInstance());
-                    password_txt.setSelection(cursor);
-                    return true;
-                case MotionEvent.ACTION_UP:
-                    password_txt.setTransformationMethod(PasswordTransformationMethod.getInstance());
-                    password_txt.setSelection(cursor);
-                    return true;
+        show_btn.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                int event = motionEvent.getAction();
+                final int cursor=password_txt.getSelectionStart();
+
+                switch (event) {
+                    case MotionEvent.ACTION_DOWN:
+                        password_txt.setTransformationMethod(null);
+                        password_txt.setSelection(cursor);
+                        return true;
+                    case MotionEvent.ACTION_CANCEL:
+                        password_txt.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                        password_txt.setSelection(cursor);
+                        return true;
+                    case MotionEvent.ACTION_UP:
+                        password_txt.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                        password_txt.setSelection(cursor);
+                        return true;
+                }
+                return true;
             }
-        return true;
-        }
 
 
 
--- app/src/main/java/com/mlabs/bbm/firstandroidapp/SignUp.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp/SignUp.java
@@ -1,17 +1,77 @@
 package com.mlabs.bbm.firstandroidapp;
 
-import android.os.Bundle;
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-
-/**
- * Created by androidstudio on 17/09/16.
- */
 public class SignUp extends AppCompatActivity {
+    LoginDataBaseAdapter loginDataBaseAdapter;
+    public final static boolean isValidEmail(CharSequence target) {
+        if (TextUtils.isEmpty(target)) {
+            return false;
+        } else {
+            return android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
+        }
+    }
+
+    private Toast popToast;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
+
+        // get Instance  of Database Adapter
+        loginDataBaseAdapter=new LoginDataBaseAdapter(this);
+        loginDataBaseAdapter=loginDataBaseAdapter.open();
+
+        final EditText remail = (EditText) findViewById(R.id.email);
+        final EditText rpass = (EditText) findViewById(R.id.pwd);
+        final EditText rconfpass = (EditText) findViewById(R.id.cpwd);
+        final Button register = (Button) findViewById(R.id.ok_btn);
+
+        popToast = Toast.makeText(getApplicationContext(), null, Toast.LENGTH_SHORT);
+
+
+        register.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                if(isValidEmail(remail.getText())==false)
+                {
+                    remail.setError("Please enter a valid email address.");
+                }
+                else if(rpass.getText().length()<8)
+                {
+                    rpass.setError("Minimum password length is at least 8 characters.");
+                }
+                else if(rpass.getText().toString().equals(rconfpass.getText().toString()))
+                {
+
+
+                    loginDataBaseAdapter.insertEntry(remail.getText().toString(),rpass.getText().toString());
+                    popToast = Toast.makeText(getApplicationContext(), null, Toast.LENGTH_SHORT);
+                    popToast.setText("Account Successfully Created ");
+                    popToast.show();
+
+                    Intent intent = new Intent(SignUp.this,MainActivity.class );
+                    startActivity(intent);
+                }
+                else
+                {
+                    rconfpass.setError("Passwords does not match");
+                }
+            }
+
+        });
     }
 
-    }
+}
