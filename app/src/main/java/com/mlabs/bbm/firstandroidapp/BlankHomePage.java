9c934aad1512f3f455b44fc67142d6b23b4c9766
Jill Bustillo <jill_penthesilea@yahoo.com>
2016-09-21 21:53:00 +0800

    added mainscreen class and layout.

---
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp/BlankHomePage.java
@@ -0,0 +1,14 @@
+package com.mlabs.bbm.firstandroidapp;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ * Created by Jill Bustillo on 9/21/2016.
+ */
+public class BlankHomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity);
+    }
+}
