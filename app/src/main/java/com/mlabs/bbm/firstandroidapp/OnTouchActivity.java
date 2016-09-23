4629e71e5328794c44bcfa29994abc323e0c0cce
Lyrahd <dharylcrisostomo@gmail.com>
2016-09-23 18:01:10 +0800

    Fix Attempt on OnTouchActivity Conflict #2

--- app/src/main/java/com/mlabs/bbm/firstandroidapp/OnTouchActivity.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp/OnTouchActivity.java
@@ -9,57 +9,55 @@ import android.widget.Toast;
 
 
 public class OnTouchActivity extends AppCompatActivity {
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-                super.onCreate(savedInstanceState);
-                setContentView(R.layout.ontouch_activity);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ontouch_activity);
 
-                final ImageView imageTouch = (ImageView) findViewById(R.id.imageTouch);
+        final ImageView imageTouch = (ImageView) findViewById(R.id.imageTouch);
 
 
-                imageTouch.setOnTouchListener(new View.OnTouchListener(){
-                        float dx,dy,ux,uy;
+        imageTouch.setOnTouchListener(new View.OnTouchListener(){
+            float dx,dy,ux,uy;
 
-                        @Override
-                        public boolean onTouch(View view, MotionEvent motionEvent) {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
 
-                                int x = motionEvent.getAction();
+                int x = motionEvent.getAction();
 
-                                switch(x){
-                                        case MotionEvent.ACTION_DOWN:
-                                                dx = motionEvent.getX();
-                                                dy = motionEvent.getY();
-                                                Toast.makeText(getApplicationContext(), "Value of X = " + dx + "Y = "+ dy, Toast.LENGTH_LONG).show();
-                                                return true;
-                                        case MotionEvent.ACTION_UP:
-                                                ux = motionEvent.getX();
-                                                uy = motionEvent.getY();
-                                                if(ux>dx){
-                                                        Toast.makeText(getApplicationContext(), "SWIPED RIGHT"+"\n"+"Value of X = " + ux + "Y = "+ uy, Toast.LENGTH_SHORT).show();
-                                                }
-                                                if(ux<dx){
-                                                        Toast.makeText(getApplicationContext(), "SWIPED LEFT"+"\n"+"Value of X = " + ux + "Y = "+ uy, Toast.LENGTH_SHORT).show();
-                                                }
-                                                if(uy<dy){
-                                                        Toast.makeText(getApplicationContext(), "SWIPED UP"+"\n"+"Value of X = " + ux + "Y = "+ uy, Toast.LENGTH_SHORT).show();
-                                                }
-                                                if(uy>dy) {
-                                                        Toast.makeText(getApplicationContext(), "SWIPED DOWN"+"\n"+"Value of X = " + ux + "Y = " + uy, Toast.LENGTH_SHORT).show();
-                                                }
-                                                return true;
-                                }
-                                return true;
+                switch(x){
+                    case MotionEvent.ACTION_DOWN:
+                        dx = motionEvent.getX();
+                        dy = motionEvent.getY();
+                        Toast.makeText(getApplicationContext(), "Value of X = " + dx + "Y = "+ dy, Toast.LENGTH_SHORT).show();
+                        return true;
+                    case MotionEvent.ACTION_UP:
+                        ux = motionEvent.getX();
+                        uy = motionEvent.getY();
+                        if(ux>dx){
+                            Toast.makeText(getApplicationContext(), "SWIPED RIGHT"+"\n"+"Value of X = " + ux + " Y = "+ uy, Toast.LENGTH_SHORT).show();
                         }
+                        if(ux<dx){
+                            Toast.makeText(getApplicationContext(), "SWIPED LEFT"+"\n"+"Value of X = " + ux + " Y = "+ uy, Toast.LENGTH_SHORT).show();
+                        }
+                        if(uy<dy){
+                            Toast.makeText(getApplicationContext(), "SWIPED UP"+"\n"+"Value of X = " + ux + " Y = "+ uy, Toast.LENGTH_SHORT).show();
+                        }
+                        if(uy>dy) {
+                            Toast.makeText(getApplicationContext(), "SWIPED DOWN"+"\n"+"Value of X = " + ux + " Y = " + uy, Toast.LENGTH_SHORT).show();
+                        }
+                        return true;
+                }
+                return true;
+            }
 
-                });
-        }
+        });
+    }
 
-        @Override
-        protected  void onPause(){
-                super.onPause();
-                finish();
-        }
+    @Override
+    protected  void onPause(){
+        super.onPause();
+        finish();
+    }
 
 }
-
-
