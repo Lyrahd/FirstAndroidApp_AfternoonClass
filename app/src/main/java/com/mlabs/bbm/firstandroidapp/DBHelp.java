6c2a57841fe4dadc3470ef802a4a802e789a56a3
Justine <justine.jay.fernandez@adamson.edu.ph>
2016-09-25 11:20:35 +0800

    Done with Login Whether Email or Username

--- app/src/main/java/com/mlabs/bbm/firstandroidapp/DBHelp.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp/DBHelp.java
@@ -1,98 +1,40 @@
 package com.mlabs.bbm.firstandroidapp;
 
-import android.app.ActionBar;
-import android.content.ContentValues;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
-import android.view.View;
-import android.widget.EditText;
-
-import java.net.ConnectException;
-import java.sql.SQLClientInfoException;
-import java.util.HashMap;
-import java.util.SimpleTimeZone;
-
 /**
  * Created by androidstudio on 17/09/16.
  */
 public class DBHelp extends SQLiteOpenHelper {
-
-    private static final String TAG = DBHelp.class.getSimpleName();
-
-    private static String DATABASE_NAME = "Users.db";
-
-    private static final int DATABASE_VERSION = 1;
-
-    private static final String TABLE_USER = "Users";
-
-    private static final String KEY_ID ="id";
-    private static final String KEY_USERNAME ="username";
-    private static final String KEY_PASSWORD ="password";
-    //private static final String KEY_CREATED_AT ="created_at";
-
-
-    public DBHelp(Context _context){
-        super(_context,DATABASE_NAME,null,DATABASE_VERSION);
+    public DBHelp(Context context, String name, CursorFactory factory, int version)
+    {
+        super(context, name, factory, version);
     }
-
-    public DBHelp(Context context, String databaseName, SQLiteDatabase.CursorFactory o, int databaseVersion) {
-        super(context, databaseName, o, databaseVersion);
-    }
-
+    // Called when no database exists in disk and the helper class needs
+    // to create a new one.
     @Override
-    public void onCreate(SQLiteDatabase sqlDB){
-        String CREATE_USER_TABLE ="CREATE TABLE " +  TABLE_USER  + "("
-                + KEY_ID + " INTEGER PRIMARY KEY,"
-                + KEY_USERNAME + " TEXT UNIQUE,"
-                + KEY_PASSWORD + " TEXT" + ")";
-                //+ KEY_CREATED_AT + " TEXT" + ")";
-        sqlDB.execSQL(CREATE_USER_TABLE);
+    public void onCreate(SQLiteDatabase _db)
+    {
+        _db.execSQL(LogInCRUD.DATABASE_CREATE);
 
-        Log.d(TAG, "Database Tables Created");
     }
-
+    // Called when there is a database version mismatch meaning that the version
+    // of the database on disk needs to be upgraded to the current version.
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS" + TABLE_USER);
+    public void onUpgrade(SQLiteDatabase _db, int _oldVersion, int _newVersion)
+    {
+        // Log the version upgrade.
+        Log.w("TaskDBAdapter", "Upgrading from version " +_oldVersion + " to " +_newVersion + ", which will destroy all old data");
 
-        onCreate(db);
+        // Upgrade the existing database to conform to the new version. Multiple
+        // previous versions can be handled by comparing _oldVersion and _newVersion
+        // values.
+        // The simplest case is to drop the old table and create a new one.
+        _db.execSQL("DROP TABLE IF EXISTS " + "TEMPLATE");
+        // Create a new one.
+        onCreate(_db);
     }
-/**
-    public void registerUser(String username, String password) {
-        SQLiteDatabase db = this.getWritableDatabase();
-
-        ContentValues values = new ContentValues();
-
-        values.put(KEY_USERNAME, username);
-        values.put(KEY_PASSWORD, password);
-        //values.put(KEY_CREATED_AT, created_at);
-
-        long id = db.insert(TABLE_USER, null, values);
-        db.close();
-
-        Log.d(TAG, "Successfully Added user: " + id);
-
-    }
-
-    public String getUserName(String UserName){
-        SQLiteDatabase db = this.getReadableDatabase();
-        String selectQuery =  "SELECT * FROM " + TABLE_USER
-                + " WHERE " +
-                KEY_USERNAME + "=?";// It's a good practice to use parameter ?, instead of concatenate string
-
-        Cursor cursor = db.rawQuery(selectQuery, new String[] {UserName});
-        if(cursor.getCount()>1){
-            cursor.close();
-            return "Does Not Exist";
-        }
-        cursor.moveToFirst();
-        String pass = cursor.getString(cursor.getColumnIndex("password"));
-        cursor.close();
-        return pass;
-    }
-    **/
 }
