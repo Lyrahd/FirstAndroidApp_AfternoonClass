279a20b9751bd788b3eb59402b1eb4c94d1c068a
DelacruzOcampo <ralph_ocampo13@yahoo.com>
2016-09-30 21:34:23 +0800

    Final

--- app/src/main/java/com/mlabs/bbm/firstandroidapp/signupactivity.java
+++ app/src/main/java/com/mlabs/bbm/firstandroidapp/signupactivity.java
@@ -1,12 +1,10 @@
 package com.mlabs.bbm.firstandroidapp;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
@@ -15,7 +13,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * Created by androidstudio on 17/09/16.
+ * Created by RalphPogi on 17/09/16.
  */
 public class signupactivity extends AppCompatActivity{
     @Override
@@ -29,6 +27,9 @@ public class signupactivity extends AppCompatActivity{
         final EditText email = (EditText) findViewById(R.id.email);
         final EditText password = (EditText) findViewById(R.id.pass);
         final EditText confirmPassword = (EditText) findViewById(R.id.cpass);
+        final EditText username = (EditText) findViewById(R.id.username);
+        final EditText firstname = (EditText) findViewById(R.id.Firstname);
+        final EditText lastname = (EditText) findViewById(R.id.Lastname);
         Button btnRegister = (Button) findViewById(R.id.btnregister);
 
         findViewById(R.id.btnregister).setOnClickListener(new View.OnClickListener() {
@@ -37,12 +38,25 @@ public class signupactivity extends AppCompatActivity{
             public void onClick(View arg0) {
                 if (isValidEmail(email.getText().toString()))
                {
-                    if (((password != null && password.length() >= 8) && password.getText().toString().equals(confirmPassword.getText().toString())))
+                    if (((lastname!=null)
+                            &&(firstname!=null)
+                            &&(username!=null)
+                            && (password != null
+                            && password.length() >= 8)
+                            && password.getText().toString().equals(confirmPassword.getText().toString())))
                     {
-                        sqlite.registerUser(email.getText().toString(), (password.getText().toString()),thisDate);
+                        sqlite.registerUser(email.getText().toString(),
+                                password.getText().toString(),
+                                thisDate,
+                                username.getText().toString(),
+                                firstname.getText().toString(),
+                                lastname.getText().toString());
                         email.setText("");
                         password.setText("");
                         confirmPassword.setText("");
+                        firstname.setText("");
+                        username.setText("");
+                        lastname.setText("");
                         Toast.makeText(getApplication(),"Registered Successful",Toast.LENGTH_SHORT).show();
                         Intent intent = new Intent(signupactivity.this,Login.class );
                         startActivity(intent);
@@ -50,8 +64,8 @@ public class signupactivity extends AppCompatActivity{
                     else
                     {
                        password.setError("Password mismatched or insufficient password length: it should be atleast 8 characters");
-                       password.setText("");
-                       confirmPassword.setText("");
+                       //password.setText("");
+                       //confirmPassword.setText("");
                        password.requestFocus();
                     }
                 }
